17190d46da35ef10cd65ca48189c916d6f05d683
Dulaj <52458441+dulaj93@users.noreply.github.com>
2022-09-16 00:38:25 +0530

    Update web_app.py

--- .github/workflows/web_app.py
+++ .github/workflows/web_app.py
@@ -15,5 +15,6 @@ def username(username):
     return f"Hi there, {username}!"
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+#     app.debug = True
+#     app.run(host='0.0.0.0', port=5000)
+    print("Hello from Dulaj")
