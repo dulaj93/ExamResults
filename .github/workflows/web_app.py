8a135bb6956ab44c388095ca4bc75a58443e3cf2
Dulaj <52458441+dulaj93@users.noreply.github.com>
2022-09-16 00:32:33 +0530

    add web_app file

---
+++ .github/workflows/web_app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Welcome to the index page'
+
+@app.route("/hi/")
+def who():
+    return 'Who are you?'
+
+@app.route("/hi/<username>")
+def username(username):
+    return f"Hi there, {username}!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
