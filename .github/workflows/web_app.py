029866c304d9846412b9f3a93196a5dc9fe39ba1
Dulaj <52458441+dulaj93@users.noreply.github.com>
2022-09-16 00:40:04 +0530

    Update web_app.py

--- .github/workflows/web_app.py
+++ .github/workflows/web_app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return 'Welcome to the index page'
+# @app.route("/")
+# def index():
+#     return 'Welcome to the index page'
 
-@app.route("/hi/")
-def who():
-    return 'Who are you?'
+# @app.route("/hi/")
+# def who():
+#     return 'Who are you?'
 
-@app.route("/hi/<username>")
-def username(username):
-    return f"Hi there, {username}!"
+# @app.route("/hi/<username>")
+# def username(username):
+#     return f"Hi there, {username}!"
 
 if __name__ == '__main__':
 #     app.debug = True
